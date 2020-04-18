e3d819d4370cb78638daaed184dd02caf7755c3b
Lahiru <lahirudesilva.17@cse.mrt.ac.lk>
2020-04-19 00:33:06 +0530

    insertions added

---
+++ database/4_insertions.sql
@@ -0,0 +1,18 @@
+
+CALL createHospital('141d2274-589c-4abc-8ad7-ec95bf6aea6c',2,'Matugama road','Kalutara','Kalutara','Kalutara','WP',ST_SetSRID(ST_MakePoint(79.985272,6.563441),4326),'nagoda@health.gov.lk','0342 222 261','0342 222 261','$2b$10$KdbtEOKD6IXfxDi9eT4n1.xA2hQFRrl116HX53DshL/XdEhVR2X9q',2,10,60,8,400);
+CALL createHospital('a576f573-948f-4725-8fa6-6aaf082555d6',2,'Galle road','Beruwala','Beruwala','Kalutara','WP',ST_SetSRID(ST_MakePoint(79.983481,6.472330),4326),'beruwala@health.gov.lk','0342 276 162','0342 276 162','$2b$10$KdbtEOKD6IXfxDi9eT4n1.xA2hQFRrl116HX53DshL/XdEhVR2X9q',3,3,10,2,40);
+CALL createHospital('221c0e31-25fc-4e50-ab80-7bf14cba4e9d',2,'Galle road','Panadura','Panadura','Kalutara','WP',ST_SetSRID(ST_MakePoint(79.906842,6.721835),4326),'panadura@health.gov.lk','0382 232 262','0382 232 262','$2b$10$KdbtEOKD6IXfxDi9eT4n1.xA2hQFRrl116HX53DshL/XdEhVR2X9q',3,10,32,5,150);
+CALL createHospital('0b0a2e9c-f8b1-4052-8d36-d5266033be4e',2,'Hospital Lane','Horana','Horana','Kalutara','WP',ST_SetSRID(ST_MakePoint(80.066589,6.710836),4326),'horana@health.gov.lk','0342 262 379','0342 262 379','$2b$10$KdbtEOKD6IXfxDi9eT4n1.xA2hQFRrl116HX53DshL/XdEhVR2X9q',3,12,43,5,170);
+CALL createHospital('a0dd4a8f-fcde-4beb-8a76-693fbbfddb5f',2,'Panadura road','Ingiriya','Ingiriya','Kalutara','WP',ST_SetSRID(ST_MakePoint(80.162022,6.741426),4326),'ingiriya@health.gov.lk','0342 269 261','0342 269 261','$2b$10$KdbtEOKD6IXfxDi9eT4n1.xA2hQFRrl116HX53DshL/XdEhVR2X9q',3,4,14,2,40);
+CALL createHospital('c8d9fc15-97d3-46e7-b317-b42980939070',2,'Hospital road, Weththewa','Matugama','Matugama','Kalutara','WP',ST_SetSRID(ST_MakePoint(80.125583,6.524031),4326),'matugama@health.gov.lk','0342 247 261','0342 247 261','$2b$10$KdbtEOKD6IXfxDi9eT4n1.xA2hQFRrl116HX53DshL/XdEhVR2X9q',3,4,14,2,40);
+CALL createHospital('e004a6e2-2473-44d2-be0b-20a38a0760fe',2,'Hospital road','Dharga town','Dharga town','Kalutara','WP',ST_SetSRID(ST_MakePoint(80.016077,6.443309),4326),'dharga@health.gov.lk','0342 275 264','0342 275 264','$2b$10$KdbtEOKD6IXfxDi9eT4n1.xA2hQFRrl116HX53DshL/XdEhVR2X9q',3,2,8,1,20);
+
+
+
+CALL createPoliceStation('60fcacf2-a96b-4fb3-b1bc-da56f5b53091',1,'Royal lane','Horana','Horana','Kalutara','WP',ST_SetSRID(ST_MakePoint(80.069006,6.714997),4326),'horana@police.lk','0342 261 222','0342 261 222','$2b$10$KdbtEOKD6IXfxDi9eT4n1.xA2hQFRrl116HX53DshL/XdEhVR2X9q','B',12,5,32,14,4);
+CALL createPoliceStation('43f7d773-d948-464c-9dc1-797f88279609',1,'B244, Keselennawa road','Anguruwatota','Anguruwatota','Kalutara','WP',ST_SetSRID(ST_MakePoint(80.058043,6.656212),4326),'anguruwatota@police.lk','0342 251 344','0342 251 344','$2b$10$KdbtEOKD6IXfxDi9eT4n1.xA2hQFRrl116HX53DshL/XdEhVR2X9q','C',5,2,12,7,2);
+CALL createPoliceStation('f8d2e725-85d5-485f-818e-8a6a2cfd55e0',1,'Halthota - Milleniya road','Milleniya','Milleniya','Kalutara','WP',ST_SetSRID(ST_MakePoint(80.020847,6.681873),4326),'milleniya@police.lk','0342 252 222','0342 252 222','$2b$10$KdbtEOKD6IXfxDi9eT4n1.xA2hQFRrl116HX53DshL/XdEhVR2X9q','C',3,2,8,4,2);
+CALL createPoliceStation('99c32d8b-4b5e-44d0-aa09-c80664044381',1,'Galle road','Wadduwa','Wadduwa','Kalutara','WP',ST_SetSRID(ST_MakePoint(79.929894,6.664616),4326),'wadduwa@police.lk','0382 232 522','0382 232 522','$2b$10$KdbtEOKD6IXfxDi9eT4n1.xA2hQFRrl116HX53DshL/XdEhVR2X9q','B',8,5,16,9,4);
+
+
+
