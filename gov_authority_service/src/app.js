24e6100690c3f35ad7f0817b596b7e45f0a5576a
Lahiru <lahirudesilva.17@cse.mrt.ac.lk>
2020-04-26 02:26:34 +0530

    client added for testing

--- gov_authority_service/src/app.js
+++ gov_authority_service/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const govRouter = require("./service.routes");
 const bodyParser = require("body-parser");
+const db = require("./service.database")
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
--- gov_authority_service/src/service.database.js
+++ gov_authority_service/src/service.database.js
@@ -1,4 +1,4 @@
-const {Pool} = require("pg");
+const {Pool, Client} = require("pg");
 
 const pool = new Pool({
   user: "commhawk",
@@ -7,5 +7,17 @@ const pool = new Pool({
   password: "password",
   port: "5432"
 });
+
+pool.query('SELECT NOW()')
+
+const client = new Client({
+  user: "commhawk",
+  host: "postgres",
+  database: "gov_auth",
+  password: "password",
+  port: "5432"
+})
+
+client.query('SELECT NOW()')
 
 module.exports = pool;
