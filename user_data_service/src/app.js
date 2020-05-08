f622b7b70f71ca069d7bdf613bc47f0710a46f5d
Lahiru <lahirudesilva.17@cse.mrt.ac.lk>
2020-05-08 13:10:36 +0530

    cors added for individual services(optional)

--- user_data_service/src/app.js
+++ user_data_service/src/app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const userRouter = require("./service.routes");
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
+
+
+
 
 app.use("/api/user",userRouter);
 
