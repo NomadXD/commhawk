5fc1a02414313707ac5ae4acbc4e6832b67675e3
Lahiru <lahirudesilva.17@cse.mrt.ac.lk>
2020-05-15 23:55:10 +0530

    jsodcs and swagger added for uds

--- user_data_service/src/app.js
+++ user_data_service/src/app.js
@@ -2,10 +2,31 @@ const express = require("express");
 const app = express();
 const userRouter = require("./service.routes");
 const bodyParser = require("body-parser");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/api/user",userRouter);
+
+const swaggerOptions = {
+    swaggerDefinition: {
+      info: {
+        title: "User API",
+        description: "User API Information",
+        contact: {
+          name: "Amazing Developer"
+        },
+        servers: ["http://localhost:3000"]
+      }
+    },
+    // ['.routes/*.js']
+    apis: ["service.routes.js"]
+  };
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api/user/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 
 module.exports = app;
